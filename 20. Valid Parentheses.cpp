8531167934b49a616fcdc92351d286aa0539c9b1
Manhbetter <113584404+Manhbetter@users.noreply.github.com>
2022-09-15 15:41:42 +0700

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,36 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> match;
+        match['('] = ')';
+        match['{'] = '}';
+        match['['] = ']';
+        
+        stack<char> stk;
+        char x;
+        for (int i=0; i<s.length(); i++) {
+            if (s[i]=='('||s[i]=='['||s[i]=='{') {
+                stk.push(s[i]);
+                continue;
+            }
+            if (stk.empty())
+                return false;
+            const char x = stk.top();
+            stk.pop();
+            if(match[x] != s[i]){
+                return false;
+            }
+        }
+        return (stk.empty());
+    }
+};
