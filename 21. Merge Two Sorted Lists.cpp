b6687d840e42e0780118136680e0c51597095fcf
Manhbetter <113584404+Manhbetter@users.noreply.github.com>
2022-09-15 18:09:39 +0700

    Update 21. Merge Two Sorted Lists.cpp

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -16,30 +16,30 @@ Return the head of the merged linked list.
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // if list1 < 2 -> node->next = list1, else = list2
+        // if list1 <= list2 -> node->next = list1, else = list2
+        
         ListNode * dummy = new ListNode();
-        ListNode *node = dummy;
+        ListNode * Cur_node = dummy;
         
         while(list1 && list2){
             if (list1->val <= list2->val){
-                node->next = list1;
+                Cur_node->next = list1;
                 list1 = list1->next;
             }
             else {
-                node->next = list2;
+                Cur_node->next = list2;
                 list2 = list2->next;
             }
-            node = node->next;
+            Cur_node = Cur_node->next;
         }
         if(list1){
-            node->next = list1;
+            Cur_node->next = list1;
         }
         if(list2){
-            node->next = list2;
+            Cur_node->next = list2;
         }
         return dummy->next;
     }
